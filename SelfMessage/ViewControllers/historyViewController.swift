8e18cdce47399cf46496057a9fdeb20af69a1485
Emmie Ohnuki <emaohn@gmail.com>
2018-10-13 02:00:58 -0400

    set up files + storyboard

---
+++ SelfMessage/ViewControllers/historyViewController.swift
@@ -0,0 +1,13 @@
+//
+//  historyViewController.swift
+//  SelfMessage
+//
+//  Created by Emmie Ohnuki on 10/13/18.
+//  Copyright © 2018 Emmie Ohnuki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class historyViewController: UIViewController {
+    
+}
---
+++ SelfMessage/ViewControllers/menuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  menuViewController.swift
+//  SelfMessage
+//
+//  Created by Emmie Ohnuki on 10/13/18.
+//  Copyright © 2018 Emmie Ohnuki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class menuViewController: UIViewController {
+    
+}
