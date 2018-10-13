feee841b5130c53c36761c4e8a8c723427f36c71
Emmie Ohnuki <emaohn@gmail.com>
2018-10-13 03:23:47 -0400

    added tableview to menuViewController

--- SelfMessage/ViewControllers/menuViewController.swift
+++ SelfMessage/ViewControllers/menuViewController.swift
@@ -9,5 +9,29 @@
 import Foundation
 import UIKit
 class menuViewController: UIViewController {
+    var favorites: [Message]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let favoriteMessages = UserDefaults.standard.value(forKey: "favoriteMessages") as? [Message] else {return }
+        self.favorites = favoriteMessages
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+}
+
+extension menuViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let favorites = self.favorites {
+            return favorites.count
+        }
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = favorites?[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageTableViewCell") as! messageTableViewCell
+    }
+    
     
 }
